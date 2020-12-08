ac4c9a5848ec29a24b319e0407e74904a32c1a48
anitalu724 <b06901011@ntu.edu.tw>
2020-12-08 14:00:04 +0800

    20201208

---
+++ examples/ms_vcf/test.sh
@@ -0,0 +1,15 @@
+perl vcf2maf.pl \
+--input-vcf VCFMAF/examples/ms_vcf/C171115D-CO31_C171115D-BiopsyLIVER_Q100_PASS_20X_autosomal_nonBlacklist_final.vcf \
+--output-maf VCFMAF/examples/ms_maf/C171115D-CO31_C171115D-BiopsyLIVER_Q100_PASS_20X_autosomal_nonBlacklist_final.maf \
+--tumor-id C171115D-CO31 \
+--normal-id C171115D-BiopsyLIVER \
+--max-filter-ac 48
+
+
+
+perl vcf2maf.pl \
+--input-vcf VCFMAF/examples/ms_vcf/C171115D-CO34_C171115D-BiopsyLIVER_Q100_PASS_20X_autosomal_nonBlacklist_final.vcf \
+--output-maf VCFMAF/examples/ms_maf/C171115D-CO34_C171115D-BiopsyLIVER_Q100_PASS_20X_autosomal_nonBlacklist_final.maf \
+--tumor-id C171115D-CO34 \
+--normal-id C171115D-BiopsyLIVER \
+--max-filter-ac 48
