10856453a8a7ec353c247b320dfdec345c41e4a1
anitalu724 <b06901011@ntu.edu.tw>
2020-12-16 15:30:06 +0800

    20201215_Fig1

--- maf_analysis.py
+++ maf_analysis.py
@@ -12,6 +12,7 @@ import argparse, textwrap
 import ast
 from src.maf_analysis import (
     CoMutAnalysis,
+    CoMutPlot,
     SigMutatedGeneDetection,
     KnownCancerGeneAnnotation,
     MutationalSignature,
@@ -30,6 +31,11 @@ def main():
     )
     parser.add_argument("-f", "--file", nargs=1, metavar="MAF file", required=True)
     parser.add_argument("-comut", "--comut_analysis", action="store_true")
+    parser.add_argument('-cmp', '--comut_plot',nargs=4, help="Three items need to be entered:\n\
+                                                              1. TSV file for data paths.\n\
+                                                              2. TSV file which contain all information for image.\n\
+                                                              3. plot color theme(0: cold, 1: warm)\n\
+                                                              4. CoMut_plot picture file name")
     parser.add_argument("-smg", "--significantly_mutated_gene", action="store_true")
     parser.add_argument("-kcga", "--known_cancer_gene_annotaiton", action="store_true")
     parser.add_argument("-ms", "--mutational_signature", nargs=2, metavar=('step_#', 'list_of_params'),
@@ -41,8 +47,7 @@ def main():
                           2. [<Rank start number>, <Rank end number>, <Epoch number>]
                         * Plotting
                           1. 2
-                          2. [Signature Number]'''))
-                        
+                          2. [Signature Number]'''))            
     parser.add_argument("-tmb","--total_mutation_burden",nargs=1,help="One item must be entered:\n \
                                                                        1. Sequencing Length\n",)
     parser.add_argument("-oncokb","--oncokb_annotator",nargs='*',help='Three items must be entered:\n \
@@ -68,25 +73,28 @@ def main():
         pic += "/"
 
     
-    # 2. Significantly mutated gene detection (Only oncodriveCLUST works until now)
+    # 1. Significantly mutated gene detection (Only oncodriveCLUST works until now)
     if args.significantly_mutated_gene:
         df = SigMutatedGeneDetection(args.file[0])
         df.oncodriveCLUST(folder)
         # df.dNdScv(folder)
-    # 3. Known cancer gene annotation
+    # 2. Known cancer gene annotation
     if args.known_cancer_gene_annotaiton:
         df = KnownCancerGeneAnnotation(args.file[0])
         df.annotation(folder)
-       
-    # 5. Mutation burden statistics
+    # 3. Mutation burden statistics
     if args.total_mutation_burden:
         df = TotalMutationBurden(args.file[0])
         df.data_analysis(folder, int(args.total_mutation_burden[0]))
-    # 1. CoMut Plot Analysis
+    # 4.1. CoMut Plot Analysis
     if args.comut_analysis:
         df = CoMutAnalysis(args.file[0])
         df.data_analysis(folder)
-    # 4. Mutational signature
+    # 4.2 CoMut Plotting
+    if args.comut_plot:
+        plot1 = CoMutPlot(args.comut_plot[0], args.comut_plot[1])
+        plot1.plot(folder, args.comut_plot[2], args.comut_plot[3])
+    # 5. Mutational signature
     if args.mutational_signature:
         df = MutationalSignature(args.file[0])
         params = ast.literal_eval(args.mutational_signature[1])
@@ -94,25 +102,24 @@ def main():
             df.data_analysis(folder, pic, params[0], params[1], params[2])
         elif args.mutational_signature[0] == '2':
             df.plotting(folder, pic, params[0])
-     
-    # 6. OncoKB Annotator
-    if args.oncokb_annotator:
-        df = OncoKBAnnotator(args.file[0])
-        if len(args.oncokb_annotator) == 5:
-            df.data_analysis(folder,args.oncokb_annotator[0],args.oncokb_annotator[1],args.oncokb_annotator[3],args.oncokb_annotator[4])
-        else:
-            df.data_analysis(folder,args.oncokb_annotator[0],args.oncokb_annotator[1],args.oncokb_annotator[3])
-        df.plotting(folder,pic, args.oncokb_annotator[2])
-    # 7. HRD score
+    # 6. HRD score
     if args.hrd_score:
         df = HRDScore(args.hrd_score[0])
         df.data_analysis(folder, args.hrd_score[1])
         df.plotting(folder, pic)
-    #8. WGD and CIN
+    # 7. WGD and CIN
     if args.wgd_cin:
         df = WGDnCIN(args.wgd_cin[0])
         df.data_analysis(folder)
         df.plotting(folder, pic)
+    # 8. OncoKB Annotator
+    if args.oncokb_annotator:
+        df = OncoKBAnnotator(args.file[0])
+        # if len(args.oncokb_annotator) == 5:
+        #     df.data_analysis(folder,args.oncokb_annotator[0],args.oncokb_annotator[1],args.oncokb_annotator[3],args.oncokb_annotator[4])
+        # else:
+        #     df.data_analysis(folder,args.oncokb_annotator[0],args.oncokb_annotator[1],args.oncokb_annotator[3])
+        df.plotting(folder,pic, args.oncokb_annotator[2])
 
 if __name__ == "__main__":
     main()
--- src/maf_analysis.py
+++ src/maf_analysis.py
@@ -22,6 +22,8 @@ import matplotlib.style
 import matplotlib.ticker as mtick
 import matplotlib.ticker as ticker
 from matplotlib.ticker import MaxNLocator
+from comut import comut
+from comut import fileparsers
 
 COLOR_MAP = ['#266199','#b7d5ea','#acc6aa','#E0CADB','#695D73','#B88655','#DDDDDD','#71a0a5','#841D22','#E08B69']
 
@@ -30,8 +32,8 @@ class SigMutatedGeneDetection:
     """Significantly mutated gene detection (Only oncodriveCLUST works until now)
 
     Arguments:
-        file {string} -- A MAF file path
-        folder {string} -- The path for every output file
+        file        {string}    -- A MAF file path
+        folder      {string}    -- The path for every output file
 
     Outputs:
         oncodriveCLUST inputs:
@@ -156,8 +158,8 @@ class KnownCancerGeneAnnotation:
     """Known cancer gene annotation
 
     Arguments:
-        file {string} -- A MAF file path
-        folder {string} -- The path for every output file
+        file        {string}    -- A MAF file path
+        folder      {string}    -- The path for every output file
 
     Outputs:
         kcga.output.maf
@@ -237,13 +239,13 @@ class TotalMutationBurden:
 
 
 
-# 4. CoMut Plot Analysis
+# 4.1 CoMut Plot Analysis
 class CoMutAnalysis:
     """CoMut plot analysis
 
     Arguments:
-        file {string} -- A MAF file path
-        folder {string} -- The path for every output file
+        file        {string}    -- A MAF file path
+        folder      {string}    -- The path for every output file
 
     Outputs:
         mutation_data.tsv
@@ -258,16 +260,9 @@ class CoMutAnalysis:
             maf = self.df
             chosen_col = maf[['Tumor_Sample_Barcode','Hugo_Symbol','Variant_Classification']]
             chosen_col = chosen_col.rename({'Tumor_Sample_Barcode':'sample', 'Hugo_Symbol':'category', 'Variant_Classification':'value'}, axis=1)
-            value_old_list = ['Missense_Mutation', 
-                              'Nonsense_Mutation',
-                              'In_Frame_Del', 
-                              'In_Frame_Ins',
-                              'Splice_Site',
-                              'Silent',
-                              'Frame_Shift_Del',
-                              'Frame_Shift_Ins',
-                              'Nonstop_Mutation',
-                              'Translation_Start_Site']
+           
+            value_old_list = ['Missense_Mutation', 'Nonsense_Mutation','In_Frame_Del', 'In_Frame_Ins','Splice_Site',
+                              'Silent','Frame_Shift_Del','Frame_Shift_Ins','Nonstop_Mutation','Translation_Start_Site']
             remove_idx = []
             for i in range(len(chosen_col['value'])):
                 if chosen_col['value'][i] not in value_old_list:
@@ -288,7 +283,11 @@ class CoMutAnalysis:
                     elif chosen_col['value'][i] == 'Translation_Start_Site':
                         chosen_col['value'][i] = 'Translation start site'
             chosen_col = chosen_col.drop(chosen_col.index[remove_idx])
-            chosen_col.to_csv(folder+"mutation_data.tsv", sep = "\t", index = False)
+            # print(chosen_col)
+            unique_chosen_col = chosen_col.drop_duplicates()
+            print(unique_chosen_col)
+            # os._exit()
+            unique_chosen_col.to_csv(folder+"mutation_data.tsv", sep = "\t", index = False)
             print(colored(("=> Generate CoMut_Analysis output files:"), 'green'))
             print(colored(("   "+folder+"mutation_data.tsv"), 'green'))
         def mutation_clonality():
@@ -308,6 +307,160 @@ class CoMutAnalysis:
             print(colored(("   "+folder+"mutation_burden.tsv"), 'green'))
         mutation_type()
         mutation_clonality() 
+
+# 4.2 CoMutPlotting
+class CoMutPlot:
+    def __init__(self, file, info):
+        print(colored(("\nStart plotting CoMut Plot...."), 'yellow'))
+        self.fd = (pd.read_csv(file, sep="\t")).to_dict('list')
+        for item in self.fd:
+            cleanedList = [x for x in self.fd[item] if str(x) != 'nan']
+            self.fd[item] = cleanedList
+        self.info = (pd.read_csv(info, sep="\t")).to_dict('list')
+        for item in self.info:
+            cleanedList = [x for x in self.info[item] if str(x) != 'nan']
+            self.info[item] = cleanedList
+    def plot(self, folder, theme, name):
+        fixed_category = ['Same Patient','Copy Number Alteration','Mutation Type','Purity','Mutation Signature','Mutation Classification','Frequency', 'Whole Genome Doubling']
+        feature_category = [item for item in self.fd if item not in fixed_category]
+        fixed_path_list = [self.fd[item][0] if len(self.fd[item]) != 0 else "" for item in fixed_category ]
+        
+        feature_path_list = [self.fd[item][0] for item in feature_category ]
+      
+        print(colored(("*   Reading Data...."), 'yellow'))
+        same_patient_df = pd.read_csv(fixed_path_list[0], sep = '\t') if fixed_path_list[0] != "" else pd.DataFrame(data={})
+        copy_num_df     = pd.read_csv(fixed_path_list[1], sep = '\t') if fixed_path_list[1] != "" else pd.DataFrame(data={})
+        mut_type_df     = pd.read_csv(fixed_path_list[2], sep = '\t') if fixed_path_list[2] != "" else pd.DataFrame(data={})
+        purity_df       = pd.read_csv(fixed_path_list[3], sep = '\t') if fixed_path_list[3] != "" else pd.DataFrame(data={})
+        mut_sig_df      = pd.read_csv(fixed_path_list[4], sep = '\t') if fixed_path_list[4] != "" else pd.DataFrame(data={})
+        mut_clone_df    = pd.read_csv(fixed_path_list[5], sep = '\t') if fixed_path_list[5] != "" else pd.DataFrame(data={})
+        freq_df         = pd.read_csv(fixed_path_list[6], sep = '\t') if fixed_path_list[6] != "" else pd.DataFrame(data={})
+        wgd_df          = pd.read_csv(fixed_path_list[7], sep = '\t') if fixed_path_list[7] != "" else pd.DataFrame(data={})
+        feature_df = [pd.read_csv(i, sep = '\t') for i in feature_path_list]
+        
+        # primary_df      = pd.read_csv(path_list[4], sep = '\t') if path_list[4] != "" else pd.DataFrame(data={})
+        # best_res_df     = pd.read_csv(path_list[5], sep = '\t') if path_list[5] != "" else pd.DataFrame(data={})
+
+        cold_set = {'mut':['#b7d5ea','#acc6aa','#266199','#E0CADB','#695D73','#B88655','#DDDDDD','#71a0a5','#841D22'],
+                    'purity':'Blues',
+                    'burden':['#266199','#b7d5ea'],
+                    'sig':['#b7d5ea','#acc6aa','#266199','#E0CADB','#695D73','#B88655','#DDDDDD','#71a0a5','#841D22'],
+                    'copy_num':['#E5E5E5','#b7d5ea','#266199', {'facecolor': 'black', 'alpha': 0.5}, '#7566AA','#000060', '#71a0a5'],
+                    'feature':['#DDDDDD','#acc6aa','#71a0a5','#77628c', '#00033F', '#E0CADB','#b7d5ea','888888'],
+                    'primary':{'Skin':'#E5E5E5', 'Acral': '#8ec6c5', 'Occult':'#6983aa', 'Mucosal': '#8566aa'},
+                    'response':{'SD': '#DDDDDD', 'MR': '#acc6aa', 'PR': '#71a0a5', 'CR':'#77628c', 'PD':  '#00033F'}}
+
+        warm_set = {'mut':['#fbc687','#D4E2C3','#ea907a','#AA1D22','#F4D764','#EBEDAA','#BBBBBB','#aacdbe','#AA7733'],
+                    'purity':'Oranges',
+                    'burden':['#ea907a','#fbc687'],
+                    'sig':['#ea907a','#fbc687','#f4f7c5','#aacdbe'],
+                    'copy_num':['#eeeeee','#EEBBBB','#AA1C19', {'facecolor': 'black', 'alpha': 0.2}, '#CCDD99','#575033'],
+                    'primary':{'Skin':'#f7e7bd', 'Acral': '#d9c6a5', 'Occult':'#a35d6a', 'Mucosal': '#c26565'},
+                    'response':{'SD': '#ea907a', 'MR': '#fbc687', 'PR': '#f4f7c5', 'CR':'#aacdbe', 'PD':  '#ACBF89'}}
+
+        used_set = cold_set if theme == "0" else warm_set
+
+    # info
+        #wgd
+        wgd_mapping = {'Yes': {'facecolor': 'black', 'alpha': 0.5}, 'No': 'white'}
+        #same patient
+        indicator_kwargs = {'color': 'black', 'marker': 'o', 'linewidth': 1, 'markersize': 5}
+        #copy number
+        cna = ['Baseline', 'Allelic amplification', 'Allelic deletion','aCN = 0', 'CN-LOH', 'Complex', 'Multiple']
+        cna_mapping = dict(zip(cna, used_set['copy_num']))
+        cna_order = self.info['Copy Number Alteration']
+        #mut
+        category_order = self.info['Mutation Type']
+        item = ['Multiple','Silent','Missense','Frameshift indel','In frame indel','Translation start site','Nonstop','Splice site','Nonsense']
+        mut_mapping = dict(zip(item, used_set['mut']))
+        #signature
+        sig_kwargs = {'edgecolor': 'white', 'width': 0.96, 'linewidth': 0.5}
+        sig = self.info['Mutation Signature']
+        if len(sig) >= 10:
+            print(colored('ERROR: The number of mutation signature must be less than 10!\n', 'red'))
+            print(colored('CoMut Plotting Halt\n', 'red'))
+            return
+        sig_mapping = {sig[i]:used_set['sig'][i] for i in range(len(sig))}
+        #burden(clonality)
+        burden = ['Nonsynonymous','Synonymous']
+        bar_mapping = dict(zip(burden, used_set['burden']))
+        bar_kwargs = {'width': 0.8}
+    
+    # features
+        feature_mapping = []
+        for feature in feature_category:
+            fea = self.info[feature]
+            if len(fea) > 8:
+                print(colored(('ERROR: The number of type of \"'+feature+"\" must be less than 8!\n"), 'red'))
+                print(colored('CoMut Plotting Halt\n', 'red'))
+                return
+            fea_mapping = {fea[i]:used_set['feature'][i] for i in range(len(fea))}
+            feature_mapping.append(fea_mapping)
+        from matplotlib import rcParams
+        custom_rcParams = {'font.family': 'arial','font.size': 14}   
+        rcParams.update(custom_rcParams)
+
+
+    # Plot
+        toy_comut = comut.CoMut()
+        toy_comut.samples = list(wgd_df['sample'])
+        width, fig_info = 0, 0
+        FigCol = 2 if width > 10 else 1
+        height = int(width/10)*8
+        # print(width)
+        FigSize = (10, 10)
+        
+        if not same_patient_df.empty:
+        #     width += 1
+        #     fig_info += 1
+            toy_comut.add_sample_indicators(same_patient_df, name = fixed_category[0], plot_kwargs = indicator_kwargs) 
+        if not wgd_df.empty:
+            toy_comut.add_categorical_data(wgd_df, name=fixed_category[7], mapping = wgd_mapping)
+
+        if not copy_num_df.empty:
+        #     width += 2
+        #     fig_info += 7
+            toy_comut.add_categorical_data(copy_num_df, name=fixed_category[1], mapping = cna_mapping, category_order = cna_order) 
+        if not mut_type_df.empty:
+            # width += 5*int(len(self.info['Mutation Type'])/10)
+            # fig_info += 9
+            toy_comut.add_categorical_data(mut_type_df, name = fixed_category[2], category_order = category_order, mapping = mut_mapping)
+        
+        if not purity_df.empty:
+        #     width += 1
+            toy_comut.add_continuous_data(purity_df, name = fixed_category[3], mapping = used_set['purity'], value_range = (0, 1))
+        
+        # for i in range(len(feature_df)):
+        #     width += 1
+        #     toy_comut.add_categorical_data(feature_df[i], name=feature_category[i], mapping = feature_mapping[i])
+        
+        if not mut_sig_df.empty:
+        #     width += 2
+            toy_comut.add_bar_data(mut_sig_df, name = fixed_category[4], mapping = sig_mapping, stacked = True, ylabel = 'Mutational\nsignatures',bar_kwargs = sig_kwargs)
+        if not mut_clone_df.empty:
+        #     # width += 2
+            toy_comut.add_bar_data(mut_clone_df, name = fixed_category[5], mapping = bar_mapping, stacked = True, bar_kwargs = bar_kwargs, ylabel = 'Muts/Mb')
+        
+        
+        toy_comut.plot_comut(figsize = FigSize, x_padding = 0.04, y_padding = 0.04, tri_padding = 0.03)
+        toy_comut.add_unified_legend(ncol = 1)
+        toy_comut.axes[fixed_category[1]].set_xticklabels([])
+        toy_comut.figure.savefig(folder+name, dpi = 300, bbox_inches = 'tight')
+        os._exit()
+        
+        # print(FigSize)
+        
+        
+        # patient_leg = toy_comut.add_axis_legend('Same Patient', bbox_to_anchor = (1.025, 1.2), frameon = False, numpoints = 2)
+        toy_comut.plot_comut(figsize = FigSize, x_padding = 0.04, y_padding = 0.04, tri_padding = 0.03)
+        toy_comut.add_unified_legend(ncol = 2)
+        toy_comut.figure.savefig(folder+name, dpi = 300, bbox_inches = 'tight')
+        print(colored(("=> Generate CoMut Plot: "+folder+name), 'green'))
+
+    
+
+
+
 
 
 # 5. Mutational signature (V)
@@ -563,6 +716,10 @@ class MutationalSignature:
             sample_list, sig_list = list(df.columns),list(df.index)
             SUM = (df.sum(axis = 0, skipna = True)).tolist()
             df = df/SUM
+            dft = df.T
+            # dft.columns = ['sample']+dft.columns
+            dft.to_csv(folder+"SigContribution.tsv",index_label='sample', sep='\t')
+            print(colored(("   "+folder+"SigContribution.tsv"), 'green'))
             ind = np.arange(df.shape[1])
             data = []
             for i in range(df.shape[0]):
@@ -570,6 +727,9 @@ class MutationalSignature:
                 data.append(d)
             fig = plt.figure(figsize=(10, 5))
             ax = fig.add_axes([0,0,1,1])
+            
+            
+
             for i in range(len(data)):
                 if i == 0:
                     ax.bar(ind, data[i], 0.8, color = COLOR_MAP[i])
@@ -912,6 +1072,7 @@ class OncoKBAnnotator:
             level_list = ['LEVEL_1']
         # Pie Plot( Total pie plot )
         size = [true_num, sample_size - true_num]
+        print(size)
         labels = "Actionable\nbiomarkers"," Current absence\n of new actionable\n biomarkers"
         fig1, ax1 = plt.subplots()
         _, _, autotexts = ax1.pie(size, labels=labels, autopct='%1.1f%%', startangle=90, colors=[COLOR_MAP[3],COLOR_MAP[4]] ,textprops={'fontsize': LABEL_SIZE})
@@ -940,8 +1101,8 @@ class OncoKBAnnotator:
                 else:
                     drug_total_dict[d] += 1
         langs,count = list(drug_total_dict.keys()), list(drug_total_dict.values())
+        print(drug_total_dict)
         freq = [i/sample_size for i in count]
-        
         fig2 = plt.figure(figsize=(10,5))
         ax2 = fig2.add_axes([0,0,1,1])
         width = 0.7
